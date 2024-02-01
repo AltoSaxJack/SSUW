7c73d32191231aee5250ca1ddd41b7799068df25
AltoSaxJack <152714000+AltoSaxJack@users.noreply.github.com>
2024-02-01 08:55:10 -0500

    Update add.php

--- admin/add.php
+++ admin/add.php
@@ -96,7 +96,7 @@ if (isset($_POST['but_logout'])) {
                             <input placeholder="email" type="email" name="email" required>
                             <input placeholder="Movie ID" type="text" name="movie_id">
                             
-                            <input placeholder="Amount" type="text" name="cash" required>
+                            <!--<input placeholder="Amount" type="text" name="cash" required>-->
 
                             <button type="submit" value="submit" name="submit" class="form-btn">ADD ENTRY</button>
                             
